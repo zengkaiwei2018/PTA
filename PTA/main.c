318f77551d2c86475ed2f7a8ff79b71e29311278
zengkaiwei2018 <1912207369@qq,com>
2019-10-29 17:39:30 +0800

    奇偶分家

--- PTA/main.c
+++ PTA/main.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int i, j;
-    char ch;
-    while (scanf("%d %c %d", &i, &ch, &j) != EOF)
-    {
-        int result;
-        switch (ch){
-            case '-':
-            {
-                result = i - j;
-                printf ("%d\n", result);  break;
-            }
-            case '+':
-            {
-                result = i + j;
-                printf ("%d\n", result);  break;
-            }
-            case '*':
-            {
-                result = i * j;
-                printf ("%d\n", result);  break;
-            }
-            case '/':
-            {
-                result = i / j;
-                printf ("%d\n", result);  break;
-            }
-            case '%':
-            {
-                result = i % j;
-                printf ("%d\n", result); break;
-            }
-                
-            default:printf("ERROR\n");
+int main(){
+    int n,x;
+    int odd=0,even=0;//奇数与偶数
+    scanf("%d",&n);
+    for(int i = 0;i < n;i++){
+        scanf("%d",&x);
+        if(x % 2 != 0){
+            odd++;
+        }else{
+            even++;
         }
-        
     }
+    printf("%d %d",odd,even);
     return 0;
-}  
+}
+
+————————————————
+版权声明：本文为CSDN博主「晔清呀!!」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/kounid/article/details/79433219
