9e0aa82272e36de903b1ee41b771e71233ed7e79
zengkaiwei2018 <1912207369@qq,com>
2019-10-29 17:26:59 +0800

    两个数的简单计算器

--- PTA/main.c
+++ PTA/main.c
@@ -1,21 +1,42 @@
-#include<stdio.h>
-int main(void) {
-    int n, score;
-    int count_A = 0, count_B = 0, count_C = 0, count_D = 0, count_E = 0;
-    scanf("%d", &n);
-    while(n--) {
-        scanf("%d", &score);
-        if(score >= 90)
-            ++count_A;
-        else if(score >= 80)
-            ++count_B;
-        else if(score >= 70)
-            ++count_C;
-        else if(score >= 60)
-            ++count_D;
-        else
-            ++count_E;
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    char ch;
+    while (scanf("%d %c %d", &i, &ch, &j) != EOF)
+    {
+        int result;
+        switch (ch){
+            case '-':
+            {
+                result = i - j;
+                printf ("%d\n", result);  break;
+            }
+            case '+':
+            {
+                result = i + j;
+                printf ("%d\n", result);  break;
+            }
+            case '*':
+            {
+                result = i * j;
+                printf ("%d\n", result);  break;
+            }
+            case '/':
+            {
+                result = i / j;
+                printf ("%d\n", result);  break;
+            }
+            case '%':
+            {
+                result = i % j;
+                printf ("%d\n", result); break;
+            }
+                
+            default:printf("ERROR\n");
+        }
+        
     }
-    printf("%d %d %d %d %d\n", count_A, count_B, count_C, count_D, count_E);
     return 0;
-}
+}  
